81f8ddb4a9e0e1b3fd2c55d126ff0285335936f8
Bryce <bdipalo@hotmail.com>
2025-03-22 13:57:54 -0700

    major changes. removing style.css in favour of tailwind. Still need to fix Reviews container for xl screens. and about us section on mobile.

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",  // Main HTML file
+    "./src/**/*.{html,js,jsx,ts,tsx}",  // Include JS, JSX, TS, TSX files inside src
+
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
